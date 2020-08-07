c972a419083566dbc509fae2a52f562baac732ee
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-08-06 22:09:21 -0400

    added working api call

--- js/javascript.js
+++ js/javascript.js
@@ -7,16 +7,29 @@ button.addEventListener('click', (e) => {
 });
 
 function getDataFromItunes() {
-    let url = "https://itunes.apple.com/search?term=" + userInput.value
+    let url = "https://itunes.apple.com/search?term=" + userInput.value + "&limit=25"
     
     fetch(url)
     .then(data => data.json() )
     .then( json => {
         console.log(json);
 
+        let songDiv = $("<div class='song'>");
+        
+        //goes through and displays all the tracks in songs array
+        for(var i = 0; i < json.length; i++) {
+            //var track name
+            var trackName = json.trackCensoredName;
+            var trackDisplay = $("<h3>").text(trackName);
+
+            songDiv.append(trackDisplay);
+
+            $(".artist-display").prepend(songDiv);
+        }
         
     })
     .catch(error => console.log(error) )
 }
+
 
 
