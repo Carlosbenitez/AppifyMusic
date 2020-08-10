63aed34f087e9b1a9208cb4303951195b1307ccb
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-08-10 01:21:32 -0400

    can know get music from the api

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,69 @@
-let button = document.querySelector("#search-btn");
-let userInput = document.querySelector("#user-input");
-let output = document.querySelector("#top100");
+$(document).ready(function() {
+    let button = document.querySelector("#search-btn");
+    let userInput = document.querySelector("#user-input");
+    let output = document.querySelector("#top100");
 
-button.addEventListener('click', (e) => {
-    getDataFromItunes();
-});
+    button.addEventListener('click', (e) => {
+        getDataFromItunes();
+    });
 
-function getDataFromItunes() {
-    let url = "https://itunes.apple.com/search?term=" + userInput.value + "&limit=25"
-    
-    fetch(url)
-    .then(data => data.json() )
-    .then( json => {
-        console.log(json);
-
-        let songDiv = $("<div class='song'>");
+    function getDataFromItunes() {
+        let queryURL = "https://itunes.apple.com/search?term=" + userInput.value + "&limit=25"
         
-        //goes through and displays all the tracks in songs array
-        for(var i = 0; i < json.length; i++) {
-            //var track name
-            var trackName = json.trackCensoredName;
-            var trackDisplay = $("<h3>").text(trackName);
+        fetch(queryURL)
+        .then(data => data.json() )
+        .then( json => {
+            console.log(json);
 
-            songDiv.append(trackDisplay);
+            
+            for(var i = 0; i <= 25; i++) {
 
-            $(".artist-display").prepend(songDiv);
-        }
-        
-    })
-    .catch(error => console.log(error) )
-}
+                //gets song name form api
+                var songName = json.results[i].trackCensoredName;
+                //gets song picture from api
+                var songImg = json.results[i].artworkUrl30;
+                //gets preview audio
+                var songAudio = json.results[i].previewUrl;
+
+                var songDiv = $("<div class='songs col-sm-12'>");
+
+                //track image
+                var img = $("<img>").addClass('song-img').attr("src", songImg);
+                songDiv.append(img);
+
+                //track name
+                var track = $("<p>").addClass("track-name").text(songName);
+                songDiv.append(track);
+
+                //preview button
+                var previewButton = $( '<button />' , { 'class': 'play-btn btn btn-default', type: 'button', id: 'test1',
+                html: '<span class="glyphicon glyphicon-arrow-left"></span>' })
+
+                var pauseButton = $( '<button />' , { 'class': 'pause-btn btn btn-default', type: 'button', id: 'test1',
+                html: '<span class="glyphicon glyphicon-arrow-left"></span>' })
+
+
+                $(previewButton).on("click", function() {
+                    console.log("Hello");
+                    songAudio.play();
+
+                    
+
+                });
+
+                songDiv.append(previewButton);
+                songDiv.append(pauseButton);
+
+                $(".artist-display").prepend(songDiv);
+            }
+
+        })
+        .catch(error => console.log(error) )
+
+    }
+
+})
+
 
 
 
