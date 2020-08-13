264f00054a8b237cd27ede46bd959e8be0ccb7d2
Carlos <carlosbenitez86753@yahoo.com>
2020-08-12 21:12:05 -0400

    displayed pictures in div from javascript and removed purple background

--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     let queryURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=a1967871154f5a6506ff8d6e6d848946&format=json";
 
     fetch(queryURL)
-    .then(data => data.json())
-    .then(json => {
-        console.log(json);
-
-        for(var i = 0; i < 10; i++) {
-            //gets img of tracks from last fm API
-            var trackImg = json.tracks.track[i].image[1];
-
-            //creates div to store track pictures
-            var topTrackDiv = $("<div class='carousel-item active'>");
-
-            var img = $("<img>").addClass("song-img").attr("src", trackImg);
-            topTrackDiv.append(img);
+        .then(data => data.json())
+        .then(json => {
+            console.log(json);
 
 
-            //prepend to top-tracks
-            $(".top-tracks").prepend(topTrackDiv);
-        }
 
-    })
-    .catch(error => console.log(error) )
+
+
+            for (var i = 0; i < 10; i++) {
+
+                console.log(json.tracks.track[i].name)
+
+                let ituneUrl = "https://itunes.apple.com/search?term=" + json.tracks.track[i].name + "&limit=10";
+                var topTrackDiv = $("<div class=top-tracks>");
+                fetch(ituneUrl)
+                    .then(data => data.json())
+                    .then(json2 => {
+                        console.log(json2);
+                        //gets img of tracks from last fm API
+                        var trackImg =  json2.results[0].artworkUrl100;
+
+                        //creates div to store track pictures
+                        
+
+                        var img = $("<img>").attr("src", trackImg);
+                        console.log(img)
+                        topTrackDiv.append(img,);
+
+
+                        //prepend to top-tracks
+                        $(".top-tracks").prepend(topTrackDiv);
+                    })
+
+            }
+
+        })
+        .catch(error => console.log(error))
 
 });
